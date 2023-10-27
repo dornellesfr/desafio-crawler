fa063fa7597f5c133251cb325012fb1b10dd3997
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-10-26 21:34:31 -0300

    remove: try except removed

--- src/request.py
+++ src/request.py
@@ -3,12 +3,12 @@ import requests
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
 
 
-def get_website(link):
-    try:
-        response = requests.get(link, headers=header, timeout=4)
-        response.raise_for_status()
-        print(response.status_code)
-        return response.text
-    except requests.HTTPError as e:
-        print(f"Error: {e}")
-        return None
+def get_website(link: str):
+    # try:
+    response = requests.get(link, headers=header, timeout=4)
+    response.raise_for_status()
+    print(response.status_code)
+    return response.text
+    # except requests.HTTPError as e:
+    #     print(f"Error: {e}")
+    #     return None
