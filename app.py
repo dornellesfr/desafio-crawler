5d96113f7aa4ba73d55c097815aa9993eb7c25f3
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-10-30 09:51:17 -0300

    chore: add main code to app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello world!")
+from bs4 import BeautifulSoup
+from src.request import get_website
+from src.scraping_movie import scraping_movie
+
+url_domain = "https://www.imdb.com"
+url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+html_content = get_website(url)
 
 
-if __name__ == "__main__":
-    main()
+soup = BeautifulSoup(html_content, "html.parser")
+movies_container = soup.find_all("li")
+
+data = []
+
+for movie in movies_container[36:286]:
+    link_movie = movie.a["href"]
+
+    data.append(scraping_movie(url_domain + link_movie))
+
+print(data)
