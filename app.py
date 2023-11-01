6321e301609413f63e313100e634e49587219d16
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-11-01 11:09:27 -0300

    feat: add export to json file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import json
 from src.request import get_website
 from src.scraping_movie import scraping_movie
 
@@ -6,15 +7,18 @@ url_domain = "https://www.imdb.com"
 url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 html_content = get_website(url)
 
+if html_content is None:
+    raise TimeoutError
 
 soup = BeautifulSoup(html_content, "html.parser")
 movies_container = soup.find_all("li")
 
-data = []
+movies = []
 
-for movie in movies_container[36:286]:
-    link_movie = movie.a["href"]
+with open('./output.json', 'w') as file:
+    for movie in movies_container[36:286]:
+        link_movie = movie.a["href"]
 
-    data.append(scraping_movie(url_domain + link_movie))
+        movies.append(scraping_movie(url_domain + link_movie))
 
-print(data)
+    json.dump(movies, file, indent=2)
