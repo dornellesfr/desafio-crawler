8e21e935fa7237d73624574364f7885466f16189
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-11-01 11:09:55 -0300

    fix: add try except to timeout erros's

--- src/request.py
+++ src/request.py
@@ -1,13 +1,22 @@
 import requests
+import logging
 
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     }
 
+logging.basicConfig(filename='scraper.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 def get_website(link: str):
-    response = requests.get(link, headers=header, timeout=4)
-    response.raise_for_status()
-    print(response.status_code)
-    return response.text
+    try:
+        response = requests.get(link, headers=header, timeout=4)
+        response.raise_for_status()
+        logger.info(f'Site status code: {response.status_code}')
+        return response.text
+    except requests.ReadTimeout:
+        response = requests.get(link, headers=header, timeout=10)
+        logger.info(f'Site status code: {response.status_code}')
+        return response.text
