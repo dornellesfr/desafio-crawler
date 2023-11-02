4bad52542876def6f1d699c369372148433ff06d
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-11-02 18:18:04 -0300

    feat: add 'screenshot' page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from bs4 import BeautifulSoup
 import json
+from bs4 import BeautifulSoup
+import pdfkit
 from src.request import get_website
 from src.scraping_movie import scraping_movie
 
-url_domain = "https://www.imdb.com"
-url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
-html_content = get_website(url)
+URL_DOMAIN = "https://www.imdb.com"
+URL = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+IMG = pdfkit.from_url(URL, 'screenshot.pdf')
+MOVIES = []
+
+html_content = get_website(URL)
 
 if html_content is None:
     raise TimeoutError
@@ -13,12 +17,10 @@ if html_content is None:
 soup = BeautifulSoup(html_content, "html.parser")
 movies_container = soup.find_all("li")
 
-movies = []
-
 with open('./output.json', 'w') as file:
     for movie in movies_container[36:286]:
         link_movie = movie.a["href"]
 
-        movies.append(scraping_movie(url_domain + link_movie))
+        MOVIES.append(scraping_movie(URL_DOMAIN + link_movie))
 
-    json.dump(movies, file, indent=2)
+    json.dump(MOVIES, file, indent=2)
