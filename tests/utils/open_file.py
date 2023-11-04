58a19e38b61f722741bb90f9240d01ea0b8cc004
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-11-04 11:00:20 -0300

    chore: add pytest and remove unnecessary files

--- tests/utils/open_file.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-def open_file(dir_: str):
-    with open(dir_, 'r') as f:
-        html_movie = f.read()
-        soup = BeautifulSoup(html_movie, 'html.parser').text
-        return soup
